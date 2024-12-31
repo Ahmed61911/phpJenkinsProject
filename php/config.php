f63346be2e5c2b8de7a7a97380891ace4ab29654
Ahmed Baba <ahmed.baba.ab.96@gmail.com>
2024-12-31 04:36:51 +0100

    different config

--- php/config.php
+++ php/config.php
@@ -1,9 +1,14 @@
 <?php
-$mysqli = new mysqli('db', 'root', '', 'registration');
+$servername = getenv('DB_HOST'); // 'db' from docker-compose
+$username = getenv('DB_USER'); // 'root' from docker-compose
+$password = getenv('DB_PASSWORD'); // Password from docker-compose
+$dbname = getenv('DB_NAME'); // 'registration' from docker-compose
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if ($mysqli->connect_error) {
-    die("Database connection failed: " . $mysqli->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-echo "Database connection successful!";
 ?>
